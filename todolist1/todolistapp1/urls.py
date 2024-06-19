9723f9a3bf796cf42a78445fe4d70b9eb4eec3f0
Sathpriyan123 <sathpriyaneshaji@gmail.com>
2024-06-19 16:53:16 +0530

    THIRD COMMIT

--- todolist1/todolistapp1/urls.py
+++ todolist1/todolistapp1/urls.py
@@ -4,6 +4,8 @@ from.import views
 
 urlpatterns= [
     path('',views.home ,name='home'),
+    path('home',views.home ,name='home'),
     path('del/<int:id>',views.delform,name='del'),
+    path('edit/<int:id>',views.edit,name='edit'),
     path('dta',views.form, name='dta'),
 ]
--- todolist1/todolistapp1/views.py
+++ todolist1/todolistapp1/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.models import User
 from. models import Task
 
@@ -15,10 +15,22 @@ def form(request):
         task=request.POST['task']
         new_task=Task(Name=name,Task=task)
         new_task.save()
-        return render(request,'home.html')
+        return redirect('/home')
+    
+def edit(request,id):
+    t=get_object_or_404(Task,id=id)
+    if request.method == 'POST':
+            name=request.POST['name']
+            task=request.POST['task']
+            t.Name=name
+            t.Task=task
+            t.save()
+            return redirect('/home')
+    return render(request,'edit.html',{'editss':t})
+    
     
 def delform(request,id):
     task=Task.objects.get(id=id)
     task.delete()
-    return redirect('home')
+    return redirect('/home')
 
